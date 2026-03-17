f09197a63ba3fe4a9c1d3df092106fbe7a84d6eb
harvseale-ai <harvseale@gmail.com>
2026-03-17 17:13:35 +0000

    Add PrismaModule to MembersModule imports

--- src/modules/members/members.module.ts
+++ src/modules/members/members.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { WpApiModule } from '@common/wp-api/wp-api.module';
+import { PrismaModule } from '@modules/prisma/prisma.module';
 import { MembersCron } from '@modules/members/service/members.cron.service';
 import { ClassificationModule } from '../classification/classification.module';
 import { OpenAIService } from '../openai/openai.service';
@@ -12,7 +13,7 @@ import { MembersNewsService } from './service/members-news.service';
 import { MembersService } from './service/members.service';
 
 @Module({
-  imports: [WpApiModule, ClassificationModule],
+  imports: [WpApiModule, ClassificationModule, PrismaModule],
   controllers: [MembersController],
   providers: [
     MembersService,
